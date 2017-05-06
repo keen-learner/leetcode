90f34e072b6dd6001c0301ffcd0373057f0188c9
keen-learner <keen-learner@github.com>
2017-05-06 18:41:50 -0400

    update 57-InsertInterval.cpp

--- 57-InsertInterval.cpp
+++ 57-InsertInterval.cpp
@@ -8,6 +8,31 @@
  * };
  */
 
+class Solution1 {
+public:
+    vector<Interval> insert(vector<Interval>& intervals, Interval newInterval) {
+        size_t i = 0;
+        vector<Interval> result;
+        // Insert intervals appeared before newInterval.
+
+        while (i < intervals.size() && newInterval.start > intervals[i].end) {
+            result.emplace_back(intervals[i++]);
+        }
+
+        // Merge intervals that overlap with newInterval.
+        while (i < intervals.size() && newInterval.end >= intervals[i].start) {
+            newInterval = {min(newInterval.start, intervals[i].start),
+                max(newInterval.end, intervals[i].end)};
+            ++i;
+        }
+        result.emplace_back(newInterval);
+
+        // Insert intervals appearing after newInterval. i.e. from i to last one
+        result.insert(result.end(), intervals.cbegin() + i, intervals.cend());
+        return result;
+    }
+};
+
 class Solution {
 public:
     vector<Interval> insert(vector<Interval>& intervals, Interval newInterval) {
@@ -26,8 +51,10 @@ public:
         }
         result.emplace_back(newInterval);
 
-        // Insert intervals appearing after newInterval.
-        result.insert(result.end(), intervals.cbegin() + i, intervals.cend());
+        // Insert intervals appearing after newInterval. i.e. from i to last one
+        for (; i < intervals.size(); i++) {
+            result.push_back(intervals[i]);
+        }
         return result;
     }
 };
