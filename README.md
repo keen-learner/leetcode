effabbf0f29256d08c6c9c1c9974a5b67e7f9d2b
keen-learner <chs360@rediffmail.com>
2017-08-02 01:05:33 -0400

    add java 189 rotate array

--- README.md
+++ README.md
@@ -30,7 +30,7 @@
 11 | 168 | [Excel Sheet Column Title](https://leetcode.com/problems/excel-sheet-column-title/) | [C++](./168-ExcelSheetColumnTitle.cpp) | num to excel col; math | | 8 June 2017 |
 12 | 171 | [Excel Sheet Column Number](https://leetcode.com/problems/excel-sheet-column-number/) | [C++](./171-ExcelSheetColumnNumber.cpp) | excel col to num; math | | 8 June 2017 |
 -- | -- | -- | -- | -- | -- | -- |
-13 | 189 | [Rotate Array](https://leetcode.com/problems/rotate-array/) | [C++](./189-RotateArray.cpp) | | | 6 May 2017 |
+13 | 189 | [Rotate Array](https://leetcode.com/problems/rotate-array/) | [C++](./189-RotateArray.cpp) [Java](./java/189-RotateArray.java)| | | 6 May 2017 |
 14 | 61 | [Rotate List](https://leetcode.com/problems/rotate-list/) | [C++](./61-RotateList.cpp) | | | 6 May 2017 |
 -- | -- | -- | -- | -- | -- | -- |
 15 | 48 | [Rotate Image](https://leetcode.com/problems/rotate-image/) | [C++](./48-RotateImage.cpp) | extra | | 18 June 2017 |
---
+++ java/189-RotateArray.java
@@ -0,0 +1,54 @@
+/*
+
+Rotate an array of n elements to the right by k steps.
+
+For example, with n = 7 and k = 3, the array [1,2,3,4,5,6,7] is rotated to [5,6,7,1,2,3,4].
+
+Note:
+Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
+
+[show hint]
+
+Hint:
+Could you do it in-place with O(1) extra space?
+
+*/
+
+public class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+        reverse(nums, 0, n-k-1);
+        reverse(nums, n-k, n-1);
+        reverse(nums, 0, n-1);
+    }
+    
+    private void reverse(int[] nums, int l, int r) {
+        while(l < r) {
+            int t = nums[l];
+            nums[l] = nums[r];
+            nums[r] = t;
+            ++l;
+            --r;
+        }
+    }
+}
+
+class Solution1 {
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        int count = 0;
+        for (int start = 0; count < nums.length; start++) {
+            int current = start;
+            int prev = nums[start];
+            do {
+                int next = (current + k) % nums.length;
+                int temp = nums[next];
+                nums[next] = prev;
+                prev = temp;
+                current = next;
+                count++;
+            } while (start != current);
+        }
+    }
+}
