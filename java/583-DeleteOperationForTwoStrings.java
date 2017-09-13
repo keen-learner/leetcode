49db4a12adca308f612ab56ae4a954844742f5ac
keen-learner <chs360@rediffmail.com>
2017-09-13 19:52:43 -0400

    add java 583 Delete Operation for Two Strings

---
+++ java/583-DeleteOperationForTwoStrings.java
@@ -0,0 +1,54 @@
+/*
+
+Given two words word1 and word2, find the minimum number of steps required to make word1 and word2 the same, where in each step you can delete one character in either string.
+
+Example 1:
+Input: "sea", "eat"
+Output: 2
+Explanation: You need one step to make "sea" to "ea" and another step to make "eat" to "ea".
+Note:
+The length of given words won't exceed 500.
+Characters in given words can only be lower-case letters.
+
+*/
+
+// DP solution
+public class Solution {
+    public int minDistance(String s1, String s2) {
+        int[] dp = new int[s2.length() + 1];
+        for (int i = 0; i <= s1.length(); i++) {
+            int[] temp=new int[s2.length()+1];
+            for (int j = 0; j <= s2.length(); j++) {
+                if (i == 0 || j == 0)
+                    temp[j] = i + j;
+                else if (s1.charAt(i - 1) == s2.charAt(j - 1))
+                    temp[j] = dp[j - 1];
+                else
+                    temp[j] = 1 + Math.min(dp[j], temp[j - 1]);
+            }
+            dp=temp;
+        }
+        return dp[s2.length()];
+    }
+}
+
+class Solution1 {
+    public int minDistance(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+        int[][] dp = new int[m+1][n+1];
+        dp[0][0] = 0;
+        for(int j = 1; j<=n; j++) dp[0][j] = j;
+        for(int i = 1; i<=m; i++) dp[i][0] = i;
+        
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(word1.charAt(i) == word2.charAt(j)) dp[i+1][j+1] = dp[i][j];
+                else {
+                    dp[i+1][j+1] = Math.min(2+dp[i][j], 1+Math.min(dp[i][j+1], dp[i+1][j]));
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
