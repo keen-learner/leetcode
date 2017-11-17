9bc3a6d6d39797ff7463058254122ef5933111ce
keen-learner <chs360@rediffmail.com>
2017-11-17 00:51:29 -0500

    add 300 Longest Increasing Subsequence

--- 300-LongestIncreasingSubsequence.cpp
+++ 300-LongestIncreasingSubsequence.cpp
@@ -4,7 +4,8 @@ Given an unsorted array of integers, find the length of longest increasing subse
 
 For example,
 Given [10, 9, 2, 5, 3, 7, 101, 18],
-The longest increasing subsequence is [2, 3, 7, 101], therefore the length is 4. Note that there may be more than one LIS combination, it is only necessary for you to return the length.
+The longest increasing subsequence is [2, 3, 7, 101], therefore the length is 4. 
+Note that there may be more than one LIS combination, it is only necessary for you to return the length.
 
 Your algorithm should run in O(n2) complexity.
 
@@ -14,7 +15,6 @@ Follow up: Could you improve it to O(n log n) time complexity?
 */
 
 // http://www.geeksforgeeks.org/longest-monotonically-increasing-subsequence-size-n-log-n/
-//Time: O(nlogn)
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) { //also giving the sequence
@@ -28,8 +28,8 @@ public:
     }
 };
 
-// Time: O(n^2)
-class Solution1 {
+// DP : Time - O(n^2)
+class Solution2 {
 public:
     int lengthOfLIS(vector<int>& nums) {
         if(nums.size() == 0) return 0;
@@ -43,6 +43,29 @@ public:
                     tempmax = max(dp[j], tempmax); 
             }
             dp[i] = 1 + tempmax;
+            res = max(res, dp[i]);
+        }
+        return res;
+    }
+};
+
+// DP : Time - O(n^2)
+class Solution1 {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        if(nums.size() == 0) return 0;
+        vector<int> dp(nums.size(), 1); // VIMP: Init 1
+        // dp[i]: LIS from 0..i s.t. LIS ends at i
+        dp[0] = 1;
+        int res = 1;
+        for(int i = 1; i<nums.size(); i++) {
+            // int tempmax = 0;
+            for(int j = i-1; j>=0; j--) {
+                if(nums[i] > nums[j])
+                    if(dp[i] < 1+dp[j]) dp[i] = 1+dp[j];
+                    // tempmax = max(dp[j], tempmax); 
+            }
+            // dp[i] = 1 + tempmax;
             res = max(res, dp[i]);
         }
         return res;
--- java/300-LongestIncreasingSubsequence.java
+++ java/300-LongestIncreasingSubsequence.java
@@ -4,7 +4,8 @@ Given an unsorted array of integers, find the length of longest increasing subse
 
 For example,
 Given [10, 9, 2, 5, 3, 7, 101, 18],
-The longest increasing subsequence is [2, 3, 7, 101], therefore the length is 4. Note that there may be more than one LIS combination, it is only necessary for you to return the length.
+The longest increasing subsequence is [2, 3, 7, 101], therefore the length is 4. 
+Note that there may be more than one LIS combination, it is only necessary for you to return the length.
 
 Your algorithm should run in O(n2) complexity.
 
@@ -15,4 +16,21 @@ Special thanks to @pbrother for adding this problem and creating all test cases.
 
 */
 
-
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        if(nums==null || nums.length == 0) return 0;
+        int n = nums.length;
+        int res = 1;
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+        for(int i = 1; i<n; i++) {
+            for(int j = i-1; j>=0; j--) {
+                if(nums[i] > nums[j]) {
+                    if(dp[i] < 1+dp[j]) dp[i] = 1+dp[j];
+                }
+            }
+            res = Math.max(res, dp[i]);
+        }
+        return res;
+    }
+}
